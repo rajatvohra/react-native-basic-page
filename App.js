04849fab690b7986f6b05cb56a0b5f8eae6d88d8
rajatvohra <f20170966@hyderabad.bits-pilani.ac.in>
2021-05-02 15:47:05 +0530

    added 3 squares to homepage

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const styles =StyleSheet.create(
+   {
+      container:{
+         backgroundColor:"white",
+         flex: 1,
+         flexDirection:"row",
+         justifyContent:"space-around",
+         alignItems:"center",
+         opacity:80
+      },
+      box:{
+         justifyContent:"space-around",
+         alignItems:"center",
+         backgroundColor:"blue",
+         width:80,
+         height:80,
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+      },
+      text:{
+         color:"white",
+      }
+   }
+)
+
+export default ()=>{
+   return (
+      <View style={styles.container}>
+         <View style={[styles.box,{backgroundColor:"orange"}]}>
+            <Text style={styles.text}>
+               1
+            </Text>
+         </View>
+         <View style={[styles.box,{backgroundColor:"green"}]}>
+            <Text style={styles.text}>
+               2
+            </Text>
+         </View>
+         <View style={styles.box}>
+            <Text style={styles.text}>
+               3
+            </Text>
+         </View>
+      </View>
+   );
+};
